33ad639442d2e81f72a9d9e00ce3290804ac68a5
RodResq <rodrigoresque@yahoo.com.br>
2022-01-30 01:33:43 -0300

    first commit

---
+++ src/main/java/br/com/redutorurlrest/redutorurlrest/RedutorUrlRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.redutorurlrest.redutorurlrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedutorUrlRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedutorUrlRestApplication.class, args);
+	}
+
+}
